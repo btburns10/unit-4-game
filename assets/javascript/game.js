dbcb6fd0588c3e7a209bc233b82f7a88c7d5d5a1
Brendan Burns <btburns10@gmail.com>
2018-06-19 01:11:33 -0400

    logic finished

---
+++ assets/javascript/game.js
@@ -0,0 +1,77 @@
+//score counter
+var counter = 0;
+var wins = 0;
+var losses = 0;
+
+//gem variables
+var gem1;
+var gem2;
+var gem3;
+var gem4;
+
+//target number
+var targetNum;
+
+//generate random number from 1-12 inclusive
+function generateRandoNum() {
+    return Math.floor((Math.random() * 12) + 1);  
+};
+
+function checkScore() {
+    if(counter === targetNum) {
+        wins += 1;
+        alert("Good job! You are a level " + wins + " Gem Master.");
+        initGame();
+    }
+    else if(counter > targetNum) {
+        losses += 1;
+        alert("Sorry try again");
+        initGame();
+    }
+}
+
+//initialize game function
+function initGame() {
+    counter = 0;
+    $("#count").text(counter);
+    targetNum = Math.floor((Math.random() * 120) + 19);
+    $("#targetNum").text(targetNum);
+    gem1 = generateRandoNum();
+    gem2 = generateRandoNum();
+    gem3 = generateRandoNum();
+    gem4 = generateRandoNum();
+}
+
+//button interactivity
+$(document).ready(function() {
+
+    initGame();
+
+    $("#btn-1").on("click", function() {
+        counter += gem1;
+        $("#count").text(counter);
+        checkScore();
+    });
+    $("#btn-2").on("click", function() {
+        counter += gem2;
+        $("#count").text(counter);
+        checkScore();
+    });
+    $("#btn-3").on("click", function() {
+        counter += gem3;
+        $("#count").text(counter);
+        checkScore();
+    });
+    $("#btn-4").on("click", function() {
+        counter += gem4;
+        $("#count").text(counter);
+        checkScore();
+    });
+
+
+
+});
+
+    
+
+
